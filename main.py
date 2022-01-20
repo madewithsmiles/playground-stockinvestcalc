cb23c4a61dc9268e44299d03964c104c059e603c
N'Godjigui Junior Diarrassouba <41027427+madewithsmiles@users.noreply.github.com>
2022-01-20 01:34:52 -0500

    adding some functions

--- main.py
+++ main.py
@@ -1,3 +1,61 @@
+from collections import namedtuple
+from curses import ERR
+
+InvestmentSuggestion = namedtuple('InvestSuggestion', 
+                                ['SharesToBuy', 
+                                'InvestmentValue', 
+                                'CurrentPrice', 
+                                'TargetBalance'])
+
+class ERR_FLAG:
+    DIV_BY_ZERO = 0x0
+
+class CalcFactory:
+    # Calculate based on
+    PREVIOUS_GROWTH = 0x0
+    TARGET_PRICE = 0x1
+    MISSED_INVESTMENT = 0x2
+    POTENTIAL_INVESTMENT = 0x3
+
+    @staticmethod
+    def get_calc_function(case):
+        if case == CalcFactory.PREVIOUS_GROWTH:
+            pass
+        if case == CalcFactory.TARGET_PRICE:
+            pass
+        if case == CalcFactory.MISSED_INVESTMENT:
+            pass
+        else:
+            pass
+    
+    def calc_invest_given_previous_growth(current_price, old_price, target_balance, verbose=False):
+        rate_of_change = get_price_change_rate_from_old_price(old_price, current_price)
+        if verbose:
+            print(f'{CalcFactory.PREVIOUS_GROWTH}: price grew by {rate_of_change:,} ({round(rate_of_change * 100, 3):,}%)')
+        next_price = current_price * rate_of_change
+        
+        if next_price == 0:
+            return ERR_FLAG.DIV_BY_ZERO
+
+        return get_suggested_investment_given_target_balance_and_target_price(current_price, next_price, target_balance)
+    
+
+
+def get_investment_value_when_price_changes(investment_amount, current_price, next_price):
+    nbr_of_shares = investment_amount / current_price
+    return nbr_of_shares * next_price
+
+def get_price_change_rate_from_old_price(old_price, current_price):
+    return current_price / old_price
+
+def get_suggested_investment_given_target_balance_and_target_price(current_price, target_price, target_balance):
+    shares_to_buy = target_balance / target_price
+    estimated_investment = current_price * shares_to_buy
+    return InvestmentSuggestion(SharesToBuy=shares_to_buy, 
+                                InvestmentValue=estimated_investment, 
+                                CurrentPrice=current_price,
+                                TargetBalance=target_balance)
+
 def main():
     current_price = 0.000_028
     target_balance = 100_000
